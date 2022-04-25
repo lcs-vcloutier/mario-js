e66fdf99276af65ef1773cbd1a18a538468acb62
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-04-25 10:20:42 -0400

    loaded sprites

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// main function
 kaboom({
     global: true,
     fullscreen: true,
@@ -5,3 +6,29 @@ kaboom({
     debug: true,
     clearColor: [0, 0, 0, 1],
   })
+
+
+// set to true so he jumps when game starts
+let isJumping = true
+
+// main world stuff
+loadRoot('https://i.imgur.com/')
+loadSprite('coin', 'wbKxhcd.png')
+loadSprite('evil-shroom', 'KPO3fR9.png')
+loadSprite('brick', 'pogC9x5.png')
+loadSprite('block', 'M6rwarW.png')
+loadSprite('mario', 'Wb1qfhK.png')
+loadSprite('mushroom', '0wMd92p.png')
+loadSprite('surprise', 'gesQ1KP.png')
+loadSprite('unboxed', 'bdrLpi6.png')
+loadSprite('pipe-top-left', 'ReTPiWY.png')
+loadSprite('pipe-top-right', 'hj2GK4n.png')
+loadSprite('pipe-bottom-left', 'c1cYSbt.png')
+loadSprite('pipe-bottom-right', 'nqQ79eI.png')
+
+// blue world stuff
+loadSprite('blue-block', 'fVscIbn.png')
+loadSprite('blue-brick', '3e5YRQd.png')
+loadSprite('blue-steel', 'gqVoI2b.png')
+loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+loadSprite('blue-surprise', 'RMqCc1G.png')
